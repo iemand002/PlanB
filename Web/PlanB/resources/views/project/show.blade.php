29578ba96945fc00923f0e8c06c98024910529cb
VanAkenKristof <kristof.vanaken@student.kdg.be>
2016-04-28 15:45:50 +0200

    Bootstrap fix 2

--- Web/PlanB/resources/views/project/show.blade.php
+++ Web/PlanB/resources/views/project/show.blade.php
@@ -11,24 +11,32 @@ Projecten
 @section('content')
 
 <h1>{{ $project->naam }}</h1>
-
 <div class="row">
-	<div class="projectAfbeelding col-sm-3">
-		{!! Html::image('/img/'.$project->milestones[0]->afbeelding,'',['class'=>'projectImage']) !!}
+</div>
+<div class="row">
+	<div class="col-sm-8">
+		<div class="projectAfbeelding col-sm-4">
+			{!! Html::image('/img/'.$project->milestones[0]->afbeelding,'',['class'=>'projectImage']) !!}
+		</div>
+		<div class="projectBeschrijving col-sm-8">
+			<p>{{ $project->beschrijving }}</p>
+			<p>Looptijd project:</p>
+			<p>van: {{ $project->publish_from->format('d-m-Y') }}</p>
+			<p>tot: {{ $project->publish_till->format('d-m-Y') }}</p>
+		</div>
 	</div>
-	<div class="projectBeschrijving col-sm-5">
-		<p>{{ $project->beschrijving }}</p>
-		<p>Looptijd project:</p>
-		<p>van: {{ $project->publish_from->format('d-m-Y') }}</p>
-		<p>tot: {{ $project->publish_till->format('d-m-Y') }}</p>
-	</div>
-	<div class="projectMilestones col-sm-4">
-		<h2>Milestones</h2>
-		@foreach ($project->milestones as $milestone)
-		<h3>{{ $milestone->naam }}</h3>
-		<p>{{ $milestone->beschrijving }}</p>
-		<p>Aangemakt op: {{ $milestone->created_at->format('d-m-Y') }}</p>
-		@endforeach
+
+	<div class="col-sm-4">
+		<div class="row">
+			<div class="projectMilestones col-sm-12">
+				<h2>Milestones</h2>
+				@foreach ($project->milestones as $milestone)
+				<h3>{{ $milestone->naam }}</h3>
+				<p>{{ $milestone->beschrijving }}</p>
+				<p>Aangemakt op: {{ $milestone->created_at->format('d-m-Y') }}</p>
+				@endforeach
+			</div>
+		</div>
 	</div>
 </div>
 <div class="row">
