0a1042c356e7385c2ec8cc228823a3cea8faba96
VanAkenKristof <kristof.vanaken@student.kdg.be>
2016-05-06 13:44:30 +0200

    Revert "Tink - Tijdlijn"
    This reverts commit cd4eebda32e55e11195359ac3fe4639cadb44f86.
    
    # Conflicts:
    #	Web/PlanB/resources/views/project/show2.blade.php

--- Web/PlanB/resources/views/project/show2.blade.php
+++ Web/PlanB/resources/views/project/show2.blade.php
@@ -1,32 +1,34 @@
 @extends('layout.main')
 
 @section('pagetitle')
-Projecten
+    Projecten
 @endsection
 
 @section('css')
-@yield('css-sub')
+    @yield('css-sub')
 @endsection
 
 @section('content')
 
-<div class="row">
-    <div class="col-sm-8">
-        <div class="row">
-            <div class="projectAfbeelding">
-                @if($project->milestones->count()!=0)
-                <img src="{{(strpos($project->milestones[0]->afbeelding,'http')===0?'':'/img').$project->milestones[0]->afbeelding}}"
-                alt="" class='projectImage'>
-                @else
-                {!! Html::image('/images/dummy.png','',['class'=>'projectImage']) !!}
-                @endif
+    <div class="row">
+        <div class="col-sm-8">
+            <div class="row">
+                <div class="projectAfbeelding">
+                    @if($project->milestones->count()!=0)
+                        <img src="{{(strpos($project->milestones[0]->afbeelding,'http')===0?'':'/img').$project->milestones[0]->afbeelding}}"
+                             alt="" class='projectImage'>
+                    @else
+                        {!! Html::image('/images/dummy.png','',['class'=>'projectImage']) !!}
+                    @endif
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-sm-12">
-                <h1>{{$project->milestones[0]->naam}}</h1>
-                <p>{{$project->milestones[0]->beschrijving}}</p>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>{{$project->milestones[0]->naam}}</h1>
+                    <p>{{$project->milestones[0]->beschrijving}}</p>
+                </div>
             </div>
+<<<<<<< HEAD
         </div>
 <!--         <div class="row">
             <div class="projectVragen col-sm-12">
@@ -42,46 +44,41 @@ Projecten
                 @endforeach
             </div>
         </div> -->
+=======
+            <div class="row">
+                <div class="projectVragen col-sm-12">
+                    <h1>Vragen</h1>
+                </div>
+            </div>
+>>>>>>> parent of cd4eebd... Tink - Tijdlijn
 
-    </div>
+        </div>
 
-    <div class="col-sm-4">
-        <div class="row">
-            <div class="col-sm-12">
-                <h2>Projectinfo</h2>
-                <h3>{{$project->naam}}</h3>
-                <div>{{$project->beschrijving}}</div>
-                <div>
-                    <p>Looptijd project:</p>
-                    <p>van: {{ $project->publish_from }}</p>
-                    <p>tot: {{ $project->publish_till }}</p>
+        <div class="col-sm-4">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2>Projectinfo</h2>
+                    <h3>{{$project->naam}}</h3>
+                    <div>{{$project->beschrijving}}</div>
+                    <div>
+                        <p>Looptijd project:</p>
+                        <p>van: {{ $project->publish_from }}</p>
+                        <p>tot: {{ $project->publish_till }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="projectMilestones col-sm-12">
+                    <h2>Milestones</h2>
+                    @foreach ($project->milestones as $milestone)
+                        <h3>{{ $milestone->naam }}</h3>
+                        <p>{{ $milestone->beschrijving }}</p>
+                        <p>Aangemakt op: {{ $milestone->created_at->format('d-m-Y') }}</p>
+                    @endforeach
                 </div>
             </div>
         </div>
-        <div class="row">
-            <div class="projectMilestones col-sm-12">
-                <div class="timeline">
-                    <div class="timeline-label">Milestones</div>
-                    <ul class="timeline-list">
-                        @foreach ($project->milestones as $milestone)
-                        <li>
-                          <div class="timeline-list-label">
-                              <time datetime="{{ $milestone->created_at}}">{{ $milestone->created_at->format('d-m-Y') }}</time>
-                          </div>
-                          <div class="card">
-                            <div class="card-body">
-                                <h3>{{ $milestone->naam }}</h3>
-                                <p>{{ $milestone->beschrijving }}</p>
-                            </div>
-                        </div>
-                    </li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
     </div>
-</div>
-</div>
 
 
 @endsection
