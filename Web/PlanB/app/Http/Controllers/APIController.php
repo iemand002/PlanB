b115f4340ebf0461f10b59ac67fed76a1c47bad4
iemand002 <ruud.lamers@live.nl>
2016-05-06 18:16:31 +0200

    css project update + api update

--- Web/PlanB/app/Http/Controllers/APIController.php
+++ Web/PlanB/app/Http/Controllers/APIController.php
@@ -11,31 +11,44 @@ use App\Http\Requests;
 
 class APIController extends Controller
 {
-    function getProject($projectid){
-        return Milestone::where('project_id',$projectid)->orderBy('created_at','desc')->with('project')->with('project.thema')->first();
+    function getProject($projectid)
+    {
+        return json_encode(Milestone::where('project_id', $projectid)->orderBy('created_at', 'desc')
+            ->with('project')
+            ->with(['project.thema' => function ($query) {
+                $query->select('id','naam');
+            }])
+            ->first());
     }
-    
-    function getAlleThemas(){
-        return Thema::all();
+
+    function getAlleThemas()
+    {
+        return json_encode(Thema::select('id', 'naam')->get());
     }
-    
-    function getProjectenOpThema($themaid){
-        return Project::where('thema_id',$themaid)->with('milestones')->get();
+
+    function getProjectenOpThema($themaid)
+    {
+        return json_encode(Project::where('thema_id', $themaid)->with('milestones')->get());
     }
-    
-    function getProjecten(){
-        return Project::with('thema')->get();
+
+    function getProjecten()
+    {
+        return json_encode(Project::with(array('thema' => function ($query) {
+            $query->select('id', 'naam');
+        }))->get());
     }
-    
-    function likeMilestone($milestoneid){
-        $milestone=Milestone::find($milestoneid);
-        $milestone->likes+=1;
+
+    function likeMilestone($milestoneid)
+    {
+        $milestone = Milestone::find($milestoneid);
+        $milestone->likes += 1;
         $milestone->save;
     }
-    
-    function dislikeMilestone($milestoneid){
-        $milestone=Milestone::find($milestoneid);
-        $milestone->dislikes+=1;
+
+    function dislikeMilestone($milestoneid)
+    {
+        $milestone = Milestone::find($milestoneid);
+        $milestone->dislikes += 1;
         $milestone->save;
     }
 }
